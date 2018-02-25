4879e3d18cf13a4365877e66a9eb9360b3ddb9a1
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-02-25 08:50:20 -0600

    Migrate and configure
    Command rails generate devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
   validates :surnames, presence: true
-  validates :email, presence: true
-  validates :password, presence: true,
-            length: { minimum: 5 }
+  # validates :email, presence: true
+  # validates :password, presence: true,
+  #           length: { minimum: 5 }
 end
--- db/migrate/20180225030311_create_users.rb
+++ db/migrate/20180225030311_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :surnames
-      t.string :email , unique: true
-      t.string :password
+      # t.string :email , unique: true
+      # t.string :password
 
       t.timestamps
     end
