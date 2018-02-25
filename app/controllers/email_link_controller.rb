8a5d742271deaa4fbef49d9ac6ee89bfebfc850f
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-02-25 10:00:52 -0600

    Add email mailer
    Command rails g mailer EmailLinkMailer

--- app/controllers/email_link_controller.rb
+++ app/controllers/email_link_controller.rb
@@ -5,11 +5,15 @@ class EmailLinkController < ApplicationController
   end
 
   def create
-    @email_link= EmailLink.generate(params[:email])
+    @email_link = EmailLink.generate(params[:email])
     if @email_link
       redirect_to new_user_session_path, notice: 'Please check your mail tray.'
     else
       redirect_to new_user_session_path, notice: 'Not exist a user associated for that email.'
     end
+  end
+
+  def validate
+
   end
 end
--- app/models/email_link.rb
+++ app/models/email_link.rb
@@ -1,14 +1,21 @@
 class EmailLink < ApplicationRecord
   belongs_to :user
+  after_create :send_email
 
   def self.generate(email)
-    @user = User.find_by_email(email: email)
-    return nil if !@user
-
-    create(user: user,expires_at: Date.today + 1.day, token: generate_token)
+    user = User.find_by_email(email)
+    if user
+      create(user: user, expires_at: Date.today + 1.day, token: generate_token)
+    else
+      return nil
+    end
   end
 
   def self.generate_token
     Devise.friendly_token.first(16)
+  end
+
+  private def send_email
+    EmailLinkMailer.sign_in_mail(self).deliver_now
   end
 end
