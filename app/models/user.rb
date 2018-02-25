4bd3794b084fb7e521e62184fedcd9aef64c6434
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-02-25 10:11:30 -0600

    Fix bug cannot load such file -- bcrypt_ext
    Add gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt' in the gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true
-  validates :surnames, presence: true
+  # validates :name, presence: true
+  # validates :surnames, presence: true
   # validates :email, presence: true
   # validates :password, presence: true,
   #           length: { minimum: 5 }
