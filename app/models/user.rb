a02ed2d38ef0670f8d6566a997ccb3d9da9870c6
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-02-24 21:51:43 -0600

    Add new user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :surnames, presence: true
+  validates :email, presence: true
+  validates :password, presence: true,
+            length: { minimum: 5 }
 end
