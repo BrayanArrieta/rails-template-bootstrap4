ed5af62b7be12fe8a27c2467028cf4f1defab3db
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-02-25 09:26:37 -0600

    Add email link controller

---
+++ app/models/email_link.rb
@@ -0,0 +1,14 @@
+class EmailLink < ApplicationRecord
+  belongs_to :user
+
+  def self.generate(email)
+    @user = User.find_by_email(email: email)
+    return nil if !@user
+
+    create(user: user,expires_at: Date.today + 1.day, token: generate_token)
+  end
+
+  def self.generate_token
+    Devise.friendly_token.first(16)
+  end
+end
